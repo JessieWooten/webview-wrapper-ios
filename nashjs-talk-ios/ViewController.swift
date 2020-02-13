5547e1f58379f5480ce34f1161a62a0db0ccee07
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-02-13 09:24:49 -0600

    added both url types from the talk.

--- nashjs-talk-ios/ViewController.swift
+++ nashjs-talk-ios/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, WKScriptMessageHandler {
     // STEP 2: DEFINE A WEBVIEW VARIABLE
     var webView:WKWebView?
     
+    // loadview() is not included in your ViewController.swift file when you create a new project. We have to add this.
     override func loadView() {
         super.loadView()
          //STEP 3: INITIALIZE A WEBVIEW
@@ -29,8 +30,11 @@ class ViewController: UIViewController, WKScriptMessageHandler {
         // INITIALIZE OUR SCRIPT TO RECEIVE JS MESSAGES FROM THE WEBVIEW
         initContentController()
         
-        // STEP 4: CREATE A URL PATH (FILE OR WEB URL) TO THE INDEX.HTML FILE
+        // STEP 4A: CREATE A FILE PATH URL TO THE INDEX.HTML FILE
         let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "build")
+        // OR STEP 4B: CREATE A WEB URL TO THE INDEX.HTML FILE
+//        let url = URL(string: "https://google.com")
+        
         // STEP 5: CONVERT THE URL TO A URLREQUEST
         let urlReq = URLRequest(url: url!);
         // STEP 6: LOAD THE URLREQUEST IN THE WEBVIEW
