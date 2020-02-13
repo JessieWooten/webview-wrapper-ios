f5ea4de247ec698bffed3601cb7dd85c990f7763
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-02-13 09:25:37 -0600

    included the directory withour html,css, and js files.

---
+++ nashjs-talk-ios/build/precache-manifest.a1c20f0895f862d971b7bd4e9e6140f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ce42de5af9d289db36b91bbfcba066",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e27489e471c3d3b8c2d7",
+    "url": "./static/css/main.b71374aa.chunk.css"
+  },
+  {
+    "revision": "fd378bb52d8532b30589",
+    "url": "./static/js/2.0fbf52d4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.0fbf52d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27489e471c3d3b8c2d7",
+    "url": "./static/js/main.5b61017b.chunk.js"
+  },
+  {
+    "revision": "5e86f7e70b02149c80e8",
+    "url": "./static/js/runtime-main.0c349de3.js"
+  }
+]);
